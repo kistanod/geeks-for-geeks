0e23b08ce8cfe20eee9532de0b11d8ba9972880c
Daniil Kistanov <daniilkistanov@csus.edu>
2018-06-20 15:57:01 -0700

    added comments

--- freq_sort.py
+++ freq_sort.py
@@ -1,19 +1,23 @@
 # https://practice.geeksforgeeks.org/problems/sorting-elements-of-an-array-by-frequency/0
+# level: difficult
 
 
 def freq_sort(lst):
-    arr = list(range(1,61))
+    # make a list of unique digits
+    arr = list(set(lst))
+    # make a dict out of values for arr
     freq_list = {key: 0 for key in arr}
+    # run through all the values and tally up
     for item in lst:
         freq_list[item] += 1
+    # sort the dict and print values
     for w in sorted(freq_list, key=freq_list.get, reverse=True):
-        if freq_list[w] == 0:
-            break
         for i in range(freq_list[w]):
             print(w, end=" ")
     print()
 
 
+# collect the input
 for t in range(int(input())):
-    N = int(input())
+    N = int(input())  # requires N for java or c problems
     freq_sort(list(map(int, input().split())))
