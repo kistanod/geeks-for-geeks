c3c0958ec0b1152e2b9720de137571d7a2c9c463
Daniil Kistanov <daniilkistanov@csus.edu>
2018-06-21 20:52:01 -0700

    added connect_ropes problem

---
+++ medium/connect_ropes.py
@@ -0,0 +1,35 @@
+# https://practice.geeksforgeeks.org/problems/minimum-cost-of-ropes/0
+# level: medium
+
+
+def connect_ropes(lengths, temp):
+    # pass the temp(no need to sort)
+    temp = temp
+
+    if len(lengths) == 1:
+        # sum of temp, not lengths
+        return sum(temp)
+
+    # because it is sorted, i can just add two
+    # at the end
+    lengths.append(lengths[0] + lengths[1])
+
+    # add that value to temp
+    temp.append(lengths[0] + lengths[1])
+
+    # delete first two!
+    lengths.remove(lengths[0])
+    lengths.remove(lengths[0])
+
+    # return the sorted vesion of lengths and regualr temp
+    return connect_ropes(list(sorted(lengths)), temp)
+
+
+# accept all inputs and then call the function
+# do not forget to pass on another list which will collect
+# the values
+for t in range(int(input())):
+    length = input()
+    ropes = list(sorted(list(map(int, input().split()))))
+    temp = []
+    print(connect_ropes(ropes, temp))
